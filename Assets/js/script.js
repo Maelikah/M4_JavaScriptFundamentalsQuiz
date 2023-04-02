14c46559cf7a6a18de66e2530bfdd9d702d692da
Maelikah <eva.longoria@zf.com>
2023-04-02 15:45:26 -0600

    Declared Question variable array, with each question as an object within the array

---
+++ Assets/js/script.js
@@ -0,0 +1,36 @@
+// Declare Array Variable that stores questions as objects
+var quizQuestions = [
+    //Object 1
+    {
+        question: "Which of the following is NOT a data type in JavaScript?",
+        choices: ["Number", "Undefined", "Object", "Null"],
+        answer: "Null", 
+    
+    },
+    //Object 2
+    {
+        question: "Which of the following is used to comment in JavaScript?",
+        choices: ["\"", "//", "<!-->", "&"],
+        answer: "//",     
+    },
+    //Object 3
+    {
+        question: "Arrays in JavaScript can be used to store which of the following?",
+        choices: ["Variables", "Objects", "Arrays", "All of the above"],
+        answer: "All of the above", 
+    },
+    //Object 4
+    {
+        question: "What does this operation === do?",
+        choices: ["Compares values", "Compares contents", "Compares values and types", "Compares types"],
+        answer: "Compares values and types", 
+    },
+    //Object 6
+    {
+        question: "Which of the following is used for debugging?",
+        choices: ["prompt", "CSS", "console log", "alert"],
+        answer: "console log", 
+    },
+]; 
+
+console.log(quizQuestions);
